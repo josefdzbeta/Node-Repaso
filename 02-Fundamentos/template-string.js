3e7ac9fefb2a6a1b5e206b126143633438403dca
josefdzbeta <jafb19@outlook.com>
2023-05-24 09:10:36 +0200

    templateLiterals

---
+++ 02-Fundamentos/template-string.js
@@ -0,0 +1,15 @@
+const nombre = 'Deadpool'
+const real = 'Wade Winston'
+
+const nombreCompleto = nombre +' '+ real;
+const nombreTemplate = `${nombre} ${real} `;
+
+console.log(nombreCompleto)
+console.log(nombreTemplate)
+
+const html =`
+<h1>Hola</h1>
+<p>Mundo</p>
+`
+
+console.log(html)
