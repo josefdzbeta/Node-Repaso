d0a40ee8ba7e9cb2195b40b5957c2030a4b9f3bc
josefdzbeta <jafb19@outlook.com>
2023-05-24 08:45:06 +0200

    javascript event loop

--- 01-hola-mundo/app3.js
+++ 01-hola-mundo/app3.js
@@ -1,9 +1,17 @@
+//JavaScript Event loop
+
 console.log('Inicio de programa')
 
 
 setTimeout( ()=> {
-
+    console.log('1')
 }, 3000)
+setTimeout( ()=> {
+    console.log('2')
+}, 0)
+setTimeout( ()=> {
+    console.log('3')
+}, 0)
 
 
 console.log('fin de programa')
