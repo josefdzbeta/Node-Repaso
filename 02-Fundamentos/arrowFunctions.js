7d3ff4a5788dc342c1ac12c5aada7f1652b61352
josefdzbeta <jafb19@outlook.com>
2023-05-24 16:06:36 +0200

    arrow functions

---
+++ 02-Fundamentos/arrowFunctions.js
@@ -0,0 +1,15 @@
+// function sumar(a,b) {
+//     return a+b
+// }
+
+// console.log(sumar(10,5))
+
+
+// const sumar = (a,b) => {
+//     return a+b
+// }
+const sumar = (a,b) => a+b
+const saludar = () => 'Hola mundo'
+
+console.log(sumar(10,9))
+console.log(saludar())
