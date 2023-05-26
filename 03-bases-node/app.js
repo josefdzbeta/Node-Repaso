6baaaffba716451ba93212058ad7af2c6659f12d
josefdzbeta <jafb19@outlook.com>
2023-05-26 14:52:27 +0200

    cli

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,21 +1,22 @@
 const {crearArchivo} = require('./helpers/multiplicar')
-
+const argv = require('yargs').argv
 console.clear()
 
-console.log(process.argv)
-const [,,arg3 = 'numero=5'] = process.argv
-const [ , numero = 5] = arg3.split('=')
+// const [,,arg3 = 'numero=5'] = process.argv
+// const [ , numero = 5] = arg3.split('=')
 
-console.log(numero)
+// console.log(numero)
 
 
 // const numero = 5
 
+console.log(process.argv)
+console.log('numero: yargs', argv.numero)
 
 
-crearArchivo( numero )
-    .then((nombreArchivo) => {
-        return console.log(nombreArchivo, 'creado')
-    }).catch((err) => {
-        return console.log(err);
-    });
+// crearArchivo( numero )
+//     .then((nombreArchivo) => {
+//         return console.log(nombreArchivo, 'creado')
+//     }).catch((err) => {
+//         return console.log(err);
+//     });
